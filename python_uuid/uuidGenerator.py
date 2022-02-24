65c89dc0702c0831be377c7fc658d940ea73716a
Eric Anderson <eric.anderson@ibotta.com>
2022-02-24 11:19:15 -0700

    python with example custom namespace uuid

--- python_uuid/uuidGenerator.py
+++ python_uuid/uuidGenerator.py
@@ -4,8 +4,10 @@ import uuid
 with open('/Users/eric.anderson/projects/github.com/ibotta/compare_uuid_gen/inputs/gtins.txt') as gtins:
     uuidOut = open('/Users/eric.anderson/projects/github.com/ibotta/compare_uuid_gen/outputs/python_uuids.txt', 'w')
     gtin = gtins.readline().strip()
+    #customUuidNamespace = uuid.uuid5(uuid.NAMESPACE_DNS, 'com.ibotta.product.gtin14')
     while gtin:
-        # do things
         uuid_result = uuid.uuid5(uuid.NAMESPACE_DNS, gtin)
+        # cust
+        #uuid_result = uuid.uuid5(customUuidNamespace, gtin)
         uuidOut.write(gtin + '; ' + str(uuid_result)+'\n')
         gtin = gtins.readline().strip()
